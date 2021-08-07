815d83967be4bda37342e97628260d99cd7fc91e
iiriis <55133414+iiriis@users.noreply.github.com>
2021-08-07 22:11:15 +0530

    Counting PushUps

--- Gym-Bot/Gym-Bot.ino
+++ Gym-Bot/Gym-Bot.ino
@@ -1,12 +1,39 @@
-void setup() {
-  pinMode(A0,INPUT_PULLUP);
-  pinMode(3,OUTPUT);
-  Serial.begin(115200);
-  
-}
+#define af 0.1
 
-void loop() {
-  analogWrite(3, 200);
-  Serial.println(analogRead(A0));
- 
+float raw = 0;
+boolean edge = true;
+
+void setup() 
+{
+  pinMode(A0, INPUT_PULLUP);
+  pinMode(3, OUTPUT);
+  Serial.begin(2000000);
+
+}
+int ct = 0;
+
+void loop()
+{
+
+  raw = af * raw + (1 - af) * analogRead(A0);
+
+  
+  //delay(10);
+
+  if (raw < 800)
+  {
+    if (edge)
+    {
+      ct++;
+      edge = false;
+    }
+  }
+
+  else if (raw >= 810)
+    edge = true;
+
+Serial.print(raw);
+Serial.print("  ");
+Serial.println(ct);
+
 }
